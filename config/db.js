8ded8752a9820ad985eeaadd2464aa1317ead67b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-21 15:02:04 +0000

    refactor connectDB with async/await

--- config/db.js
+++ config/db.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
-    })
-    .then(() => console.log('MongoDB connected.'))
-    .catch(err => {
-      console.error(err.message);
-      process.exit(1); // exit with failure
     });
+
+    console.log('MongoDB connected.');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // exit with failure
+  }
 };
 
 module.exports = connectDB;
