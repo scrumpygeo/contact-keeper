48be215a94e33eced257391af8143f131f519b1e
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-22 12:59:08 +0000

    hash password and register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -19,12 +20,42 @@ router.post(
       'Please enter password with 6 or more characters.'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed'); // test validation eg w and w/o password
+    // res.send('passed'); // = test validation eg w and w/o password
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists.' });
+      }
+
+      // if user doesn't exist,
+      user = new User({
+        name,
+        email,
+        password
+      });
+      // at this stage user object created but not saved in db
+      // We need to encrypt pwd first
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // now save in db
+      await user.save();
+      // next, send back a json web token
+      // first check if data put in db:
+      res.send('User saved.');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
