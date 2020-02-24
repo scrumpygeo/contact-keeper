61dd56a3475046024668134264464ea08d2a37a9
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-24 16:28:12 +0000

    add filter capability

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
         type: 'business'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -71,19 +72,28 @@ const ContactState = props => {
   };
 
   // Filter contacts action
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // Clear filter action
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER }); // no payload as setting back to null
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
