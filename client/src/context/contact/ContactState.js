815dc0b48f1e62bd723fa4be0066b0c725c2c15b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-27 16:33:44 +0000

    get and clear contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,15 +1,16 @@
 import React, { useReducer } from 'react';
 import axios from 'axios';
-import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CLEAR_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR
 } from '../types';
@@ -17,13 +18,26 @@ import {
 // create initial state; initially hard code data for dev
 const ContactState = props => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
     filtered: null,
     error: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // GET Contacts action
+  const getContacts = async () => {
+    // no config bcos not sending data as it's a get request
+
+    try {
+      const res = await axios.get('/api/contacts');
+
+      dispatch({ type: GET_CONTACTS, payload: res.data }); //res.data is from server and is new added contact
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
 
   // Add Contact action
   const addContact = async contact => {
@@ -45,6 +59,11 @@ const ContactState = props => {
   // Delete Contact action
   const deleteContact = id => {
     dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  // Clear contacts action
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS }); // no payload as setting back to null
   };
 
   // Set Current Contact action
@@ -85,7 +104,9 @@ const ContactState = props => {
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        getContacts,
+        clearContacts
       }}
     >
       {props.children}
