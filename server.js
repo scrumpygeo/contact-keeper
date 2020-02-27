d1a9bec3ac945b4f3079b2cf9de33454e0c3e8b0
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-27 19:37:35 +0000

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,14 +10,26 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to ContactVault, the Contact Keeper API.' })
+app.get(
+  '/',
+  (req, res) =>
+    res.json({ msg: 'Welcome to ContactVault, the Contact Keeper API.' }) //remove for production?
 );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Server static assets (ie React) in production
+if (process.env.NODE_ENV === 'production') {
+  // set a static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
